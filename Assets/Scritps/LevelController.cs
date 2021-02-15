773ed07eeb9e76d591970b8bb57ca1e5f9057cf2
YonHoshihara <hoshiharyon@gmail.com>
2021-02-15 18:06:15 -0300

    Add Game Over System

--- Assets/Scritps/LevelController.cs
+++ Assets/Scritps/LevelController.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public TimeController time_controller;
+    public GameObject game_over_feedback;
+    public bool game_over;
+    
+    public IEnumerator Start()
+    {
+        game_over = false;
+        while (true)
+        {
+            if (time_controller.time_end)
+            {
+                Debug.Log(time_controller.time_end);
+                GameOver();
+            }
+            yield return new WaitForSeconds(.01f);
+        }
+
+    }
+
+    // Update is called once per frame
+    
+    public void GameOver()
+    {
+        game_over = true;
+        game_over_feedback.SetActive(true);
+    }
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadScene(string scene)
     {
-        
+        SceneManager.LoadScene(scene);
     }
 }
--- Assets/Scritps/Moviment.cs
+++ Assets/Scritps/Moviment.cs
@@ -23,59 +23,70 @@ public class Moviment : MonoBehaviour
     [Range(0.1f,2.0f)]
     public float min_conector_distance;
 
+    public LevelController level_controller;
+
     // Start is called before the first frame update
     void Start()
     {
      rigidbdy = transform.root.GetComponent<Rigidbody2D>();
      rigidbdy.gravityScale = 1;
      Physics.IgnoreLayerCollision(8, 8);
-        collider = GetComponent<PolygonCollider2D>(); 
+        collider = GetComponent<PolygonCollider2D>();
+     
      
     }
-
     void OnMouseDown() {
+
+        if (!level_controller.game_over)
+        {
+            start_position = transform.root.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            rigidbdy.gravityScale = 0;
+            is_moving = true;
+            is_conected = false;
+        }
         
-        start_position = transform.root.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        rigidbdy.gravityScale = 0;
-        is_moving = true;
-        is_conected = false; 
     }
-
     void OnMouseDrag() {
-        end_position = start_position + Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        direction = end_position - transform.root.position;
-        rigidbdy.velocity = direction * velocity;
-    }
 
-    void OnMouseUp()
-    {
-        rigidbdy.gravityScale = 1;
-        is_moving = false;
-    }
-    
-    void FixedUpdate()
-    {
-        if(!is_moving && !is_conected ){
-            distance = Vector2.Distance(transform.root.position, conector.position);
-            if(distance< min_conector_distance){
-                collider.isTrigger = true;
-                rigidbdy.gravityScale = 0;
-                rigidbdy.velocity = Vector2.zero;
-                rigidbdy.isKinematic = false;
-                transform.root.position = Vector2.MoveTowards(transform.root.position, conector.position,0.2f);    
-            }
-            
-            if(distance< 0.01){
-                is_conected = true;
-                transform.root.position = conector.position;
-                GameObject obj = conector.gameObject;
-                obj.GetComponent<SpriteRenderer>().enabled = false;
-            }
+        if (!level_controller.game_over)
+        {
+            end_position = start_position + Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction = end_position - transform.root.position;
+            rigidbdy.velocity = direction * velocity;
         }
     }
-
-    void Update()
+    void OnMouseUp()
     {
-        
+        if (!level_controller.game_over)
+        {
+            rigidbdy.gravityScale = 1;
+            is_moving = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (!level_controller.game_over)
+        {
+            if (!is_moving && !is_conected)
+            {
+                distance = Vector2.Distance(transform.root.position, conector.position);
+                if (distance < min_conector_distance)
+                {
+                    collider.isTrigger = true;
+                    rigidbdy.gravityScale = 0;
+                    rigidbdy.velocity = Vector2.zero;
+                    rigidbdy.isKinematic = false;
+                    transform.root.position = Vector2.MoveTowards(transform.root.position, conector.position, 0.2f);
+                }
+
+                if (distance < 0.01)
+                {
+                    is_conected = true;
+                    transform.root.position = conector.position;
+                    GameObject obj = conector.gameObject;
+                    obj.GetComponent<SpriteRenderer>().enabled = false;
+                }
+            }
+        }
     }
 }
